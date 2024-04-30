7bb31783e60303b498021cd01a449229ac9d059d
iden-a <idenamoako@gmail.com>
2024-04-30 11:25:13 -0400

    fixing console formatting

--- Project1/tictactoe.cpp
+++ Project1/tictactoe.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
 
-
 void displayBoard(char [][3], int ,int);
 void getPlayerChoice(char [][3]);
-bool playerWinOrLose(char [][3], char);
-void getResult(char [][3]);
-void resetBoard(char [][3], int ,int);
-void displayMenu();
+bool playerWinOrLose(char [][3], char); 
+void getResult(char [][3]); 
+void resetBoard(char [][3], int ,int); 
+void displayMenu(); 
 
 int main() {
 
-    char board[3][3] = {
-        {'_', '_', '_'},
-	    {'_', '_', '_'},
-	    {'_', '_', '_'}
-    };
+char board[3][3] = { { ' ', ' ', ' ' }, 
+                     { ' ', ' ', ' ' }, 
+                     { ' ', ' ', ' ' } }; 
 
     // create a display menu function to allows user to enter the game, exit the game
     int choice;
@@ -32,11 +29,11 @@ int main() {
 
             case 2:
             cout << endl;
-            cout << " Exiting the Game!" << endl;
+            cout << "\tExiting the Game!" << endl;
             break;
 
             default:
-            cout << " Invalid Choice" << endl;
+            cout << "\tInvalid Choice" << endl;
         }
     
     } while( choice != 2);
@@ -46,24 +43,28 @@ int main() {
 
 void displayBoard(char board[][3], int rows, int cols) {
 
-    cout << " Player 1 - X " << endl;
-    cout << " Player 2 - O " << endl << endl;
+    cout << "\tPlayer 1 - X " << endl;
+    cout << "\tPlayer 2 - O " << endl << endl;
+    cout << ""; // Add a tab before each row
+    cout << "\t-------------" << endl;
     for (int i = 0; i < rows; i++)
 	    {
-            cout << "\t"; // Add a tab before each row
-		    for (int j = 0; j < cols; j++)
-		    {
-			    cout << board[i][j] << " ";
+            cout << "\t| "; // add a tab before each row
+
+		    for (int j = 0; j < cols; j++) {
+			    cout << board[i][j] << " | ";
 		    }
-		    cout << endl << endl;
+		    cout << endl;
+            cout << "\t-------------" << endl;
 
 	    }
+       
+
 	    return;
 }
 
-
 bool playerWinOrLose(char board[][3], char player) {
-    /*
+    /*  EXAMPLE BOARD W/ INDEX
         0 [0 1 2] 
         1 [0 1 2]
         2 [0 1 2]
@@ -97,12 +98,12 @@ bool playerWinOrLose(char board[][3], char player) {
 
 }
 
-void getResult(char board[][3]){
+void getResult(char board[][3]) {
 
     bool boardFilled = true; // setting the condition to true 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            if (board[i][j] == '_') { // if we see the the board is empty 
+            if (board[i][j] == ' ') { // if we see the the board is empty 
                 boardFilled = false; // we change the condition to false 
                 break; // this is exiting the inner loop
             }
@@ -112,23 +113,21 @@ void getResult(char board[][3]){
     // calling the playerWinLose function to get the result  
     // in case of win, lose, or draw we want to reset the board so another game can occur
     if (playerWinOrLose(board, 'X')) {
-        cout << " Player 1 - Team X Wins!" << endl;
+        cout << "\tPlayer 1 - Team X Wins!" << endl;
         resetBoard(board, 3,3);
     }
     else if (playerWinOrLose(board, 'O')) {
-        cout << " Player 2 - Team O Wins!" << endl;
+        cout << "\tPlayer 2 - Team O Wins!" << endl;
         resetBoard(board, 3,3);
     }
-    else if (boardFilled)  {
-        cout << " Draw! " << endl;
+    else if (boardFilled) {
+        cout << "\tDraw! " << endl;
         resetBoard(board, 3,3);
     }
     else {
-        // cout << "Continue Playing! (1 - True | 0 - False): " << boardFilled << endl;
-        cout << " Continue Playing!" << endl;
+        cout << "\tContinue Playing!" << endl;
     }
-};
-
+}
 
 void getPlayerChoice(char board[][3]) {
 
@@ -139,13 +138,13 @@ void getPlayerChoice(char board[][3]) {
     do {
         cout << endl;
         // check to see which player it is 
-        cout << " Player " << player << " Go!" << endl;
+        cout << "\tPlayer " << player << " Go!" << endl;
 
         // get the row
-        cout << " Enter Row: ";
+        cout << "\tEnter Row: ";
         cin >> user_row;
         // get the column 
-        cout << " Enter Column: ";
+        cout << "\tEnter Column: ";
         cin >> user_col;
 
         // making the user_row and user_col match the proper index position
@@ -154,11 +153,10 @@ void getPlayerChoice(char board[][3]) {
 
         // make sure the user inputted row and column does not exceed the board values 
         // if the board[row][col] != '-' then it must be equal to X or O, placing a value there would be invalid
-        if (user_row < 0 || user_row >= 3 || user_col < 0 || user_col >= 3 || board[user_row][user_col] != '_' ) {
-            cout << " Invalid Move. Try Again." << endl;
+        if (user_row < 0 || user_row >= 3 || user_col < 0 || user_col >= 3 || board[user_row][user_col] != ' ' ) {
+            cout << "\tInvalid Move. Try Again." << endl;
             continue;
         }
-
 
         // update the board with the player that is currently playing
         // if it is not player 1 then it must be player 2
@@ -176,17 +174,17 @@ void getPlayerChoice(char board[][3]) {
         
     // the loop will run as long as player X has not won and play O has not won
     // it will also continue as long as there is no space left on the board
-    } while (!playerWinOrLose(board, 'X') && !playerWinOrLose(board, 'O') && (board[user_row][user_col] != '_'));
+    } while (!playerWinOrLose(board, 'X') && !playerWinOrLose(board, 'O') && (board[user_row][user_col] != ' '));
 
 }
 
 void resetBoard(char board[][3], int rows, int cols) {
-
+    // resetting the board 
         for (int i = 0; i < rows; i++)
 	    {
 		    for (int j = 0; j < cols; j++)
 		    {
-			    board[i][j] = '_';
+			    board[i][j] = ' ';
 		    }
 		    cout << endl;
 	    }
@@ -195,12 +193,12 @@ void resetBoard(char board[][3], int rows, int cols) {
 
 void displayMenu() {
     cout << endl;
-    cout << "-----------------------------" << endl;
-    cout << " -- Tic-Tac-Toe Dashboard -- " << endl;
+    cout << "\t-----------------------------" << endl;
+    cout << "\t-- Tic-Tac-Toe Dashboard -- " << endl;
     cout << endl;
-    cout << " 1. Play!" << endl;
-    cout << " 2. Quit. " << endl;
+    cout << "\t1. Play!" << endl;
+    cout << "\t2. Quit. " << endl;
     cout << endl;
-    cout << "-----------------------------" << endl;
-    cout << " Choice: ";
+    cout << "\t-----------------------------" << endl;
+    cout << "\tChoice: ";
 }
